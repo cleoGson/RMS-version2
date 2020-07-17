49ded8a63405eca6269fbb4325ad91057449e88d
Frank <gfrankgef@gmail.com>
2020-07-17 15:27:53 +0300

    changes made

--- resources/views/generals/familymembers/form.blade.php
+++ resources/views/generals/familymembers/form.blade.php
@@ -2,12 +2,12 @@
             {{ Form::inputText('firstname') }}
             {{ Form::inputText('middlename') }}
             {{ Form::inputText('lastname') }}
+            {{ Form::inputSelect('relationship', $relationship) }}
             {{ Form::inputSelect('sex',[''=>'select here','male'=>'Male','female'=>'Female']) }}
             {{ Form::inputText('phone_no') }}
-            {{ Form::inputTextArea('address') }}
             {{ Form::inputDate('birth_date') }}
             {{ Form::inputSelect('disability',$disability) }}
             {{ Form::inputText('email') }}
-            {{ Form::inputSelect('relationship', $relationship) }}
+            {{ Form::inputTextArea('address') }}
       
            
